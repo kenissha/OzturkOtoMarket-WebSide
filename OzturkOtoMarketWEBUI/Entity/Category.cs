bca084f5130afbb528bd7e09e74571e66a93f000
Kenissha <rifatozturk061@gmail.com>
2023-12-13 04:20:46 +0300

    Update

--- OzturkOtoMarketWEBUI/Entity/Category.cs
+++ OzturkOtoMarketWEBUI/Entity/Category.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace OzturkOtoMarketWEBUI.Entity
 {
@@ -10,7 +12,11 @@ namespace OzturkOtoMarketWEBUI.Entity
 
         public int Id { get; set; }
 
+       
+        [DisplayName("Kategori Adı")]
+        [StringLength( maximumLength:20, ErrorMessage = "En fazla 20 karakter kullanınız")]
         public string Name { get; set;}
+        [DisplayName("Açıklama")]
         public string Description { get; set;}
 
         public List<Product> Products { get; set;}
--- OzturkOtoMarketWEBUI/Entity/Product.cs
+++ OzturkOtoMarketWEBUI/Entity/Product.cs
@@ -2,19 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel;
 
 namespace OzturkOtoMarketWEBUI.Entity
 {
     public class Product
     {
         public int Id { get; set; }
-
+        [DisplayName("Ürün Adı")]
         public string Name { get; set;}
+        [DisplayName("Ürün Açıklaması")]
         public string Description { get; set;}
+        [DisplayName("Ürün Fiyatı")]
         public string Price { get; set; }
+        [DisplayName("Ürünün Stok Adeti")]
         public int Stock { get; set; }
+        [DisplayName("Ürünün Fotoğrafı")]
         public string Image { get; set; }
-        
+
+        [DisplayName("Depoda Var mı ")]
         public bool IsApproved { get; set; }
 
         public int  CategoryId { get; set; }  
